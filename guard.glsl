6d7f1472fb33d56277d4f9552d1d891b01ab9269
Yuichiroh Arai <yuichiroh.arai@gmail.com>
2018-04-26 16:29:45 +0900

    add guard.glsl In case of a macro redefined

---
+++ guard.glsl
@@ -0,0 +1,15 @@
+#ifndef PI
+#define PI 3.141592653589793
+#endif
+
+#ifndef PI_2
+#define PI_2 6.283185307179586
+#endif
+
+#ifndef PI_H
+#define PI_H 1.5707963267948966
+#endif
+
+#ifndef PI_Q
+#define PI_Q 0.7853981633974483
+#endif
